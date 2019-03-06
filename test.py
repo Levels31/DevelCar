f783dda38a969d322581701d6ac3b17c77239640
Levels31 <Leonhard.Zierer@icloud.com>
2019-03-06 21:27:36 +0100

    updatet test

--- test.py
+++ test.py
@@ -9,37 +9,37 @@ gpio.setup(15, gpio.OUT)
 gpio.setup(12, gpio.OUT)
 gpio.setup(35, gpio.OUT)
 
+gpio.setwarnings(False)
+
 rightWheel = gpio.PWM(12, 50) # GPIO 12 als PWM mit 50Hz
 leftWheel = gpio.PWM(35, 50) # GPIO 33 als PWM mit 50Hz
 
-leftWheel.start(0)
+leftWheel.start(50)
 rightWheel.start(0)
 
-def rightWheelForward(speed):
-    gpio.output(16, True)
-    gpio.output(18, False)
+def rightWheelReverse(speed):
+    gpio.output(16, gpio.HIGH)
+    gpio.output(18, gpio.LOW)
     rightWheel.ChangeDutyCycle(speed)
 
-def rightWheelReverse(speed):
-    gpio.output(16, False)
-    gpio.output(18, True)
+def rightWheelForward(speed):
+    gpio.output(16, gpio.LOW)
+    gpio.output(18, gpio.HIGH)
     rightWheel.ChangeDutyCycle(speed)
     
 def leftWheelForward(speed):
-    gpio.output(22, True)
-    gpio.output(15, False)
+    gpio.output(22, gpio.HIGH)
+    gpio.output(15, gpio.LOW)
     leftWheel.ChangeDutyCycle(speed)
-
+    
 def leftWheelReverse(speed):
-    gpio.output(22, False)
-    gpio.output(15, True)
+    gpio.output(22, gpio.LOW)
+    gpio.output(15, gpio.HIGH)
     leftWheel.ChangeDutyCycle(speed)
-
 
 try:
     while(1):
-        rightWheelForward(50)
-        leftWheelForward(50)
+        rightWheelReverse(0)
 except KeyboardInterrupt:
     gpio.cleanup()
 
